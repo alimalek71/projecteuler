349ab4fc30d470145e07301a1d5c8bffb8195fb7
Ali Malek <ali.malek.71@gmail.com>
2020-02-14 19:57:50 +0330

    euler2: done

---
+++ src/main/java/ir/alimalek/exercise/Euler002.java
@@ -0,0 +1,25 @@
+package ir.alimalek.exercise;
+
+public class Euler002 {
+
+    public static void main(String[] args) {
+
+        long fib1 = 1;
+        long fib2 = 1;
+        long next;
+        long sum = 0;
+        int counter = 1;
+
+        do {
+            next = fib1 + fib2;
+            fib1 = fib2;
+            fib2 = next;
+            System.out.println((counter++) + ". fib1: " + fib1 + ", fib2: " + fib2);
+            if (next % 2 == 0)
+                sum += next;
+        } while (next < 4_000_000);
+
+        System.out.println("Sum of the even-valued terms: " + sum);
+    }
+
+}
