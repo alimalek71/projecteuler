42bd8894cb01d2c45112f9949a07592776ad5f3a
Ali Malek <ali.malek.71@gmail.com>
2020-02-16 22:16:44 +0330

    euler9: remove unnecessary variable

--- src/main/java/ir/alimalek/euler/Euler009_SpecialPythagoreanTriplet.java
+++ src/main/java/ir/alimalek/euler/Euler009_SpecialPythagoreanTriplet.java
@@ -3,11 +3,9 @@ package ir.alimalek.euler;
 public class Euler009_SpecialPythagoreanTriplet {
 
     public static void main(String[] args) {
-        int counter = 0;
         for (int a = 1; a < 334; a++) {
             for (int b = a + 1; b < 500; b++) {
                 for (int c = b + 1; c < 500; c++) {
-                    counter++;
                     if (a + b + c != 1000)
                         continue;
                     if (a * a + b * b != c * c)
@@ -15,7 +13,6 @@ public class Euler009_SpecialPythagoreanTriplet {
 
                     System.out.println("a: " + a + ", b: " + b + ", c: " + c);
                     System.out.println("abc: " + a * b * c);
-                    System.out.println("counter: " + counter);
                 }
             }
         }
