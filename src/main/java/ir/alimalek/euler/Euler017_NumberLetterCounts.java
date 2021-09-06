deb1dfdfa006a20d7a57bbd41185e74a382ade8e
Ali Malek <ali.malek.71@gmail.com>
2021-09-06 15:28:14 +0430

    euler17: done

---
+++ src/main/java/ir/alimalek/euler/Euler017_NumberLetterCounts.java
@@ -0,0 +1,19 @@
+package ir.alimalek.euler;
+
+public class Euler017_NumberLetterCounts {
+
+    public static void main(String[] args) {
+        System.out.println("I'd rather to calculate this problem by pen and paper, result is: 21124");
+        System.out.println("count 1-9");
+        System.out.println("3 + 3 + 5 +4 + 4 +3 +5 +5 +4 = 36");
+        System.out.println("count 10-20");
+        System.out.println("3 + 6 + 6 + 8 + 8 + 7 + 7 + 9 + 8 + 8 = 70");
+        System.out.println("count 20-99")   ;
+        System.out.println("10*(6 + 6 + 5 + 5 + 5 + 7 + 6 + 6) + 8*36 = 748");
+        System.out.println("count 100-999");
+        System.out.println("3600 + 7686 + 63 + 8910 = 20259");
+        System.out.println("sum all above + 11 (1000)");
+        System.out.println("36 + 70 + 748 + 20259 + 11 = 21124");
+    }
+
+}
